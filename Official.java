6a624276080a4c7465b13c38e6b41d8578dde141
rmits3520987YiYang <s3520987@student.rmit.edu.au>
2017-04-06 20:35:46 +1000

    Add files via upload

---
+++ Official.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Official extends Participant {
+	//constructor
+	Official(String ID, String name, int age, String state) {
+		super(ID, name, age, state);
+	}
+
+	/*
+	 * call the time list of every athlete,and return a int[i]
+	 * i is the length of the ArrayList
+	 * the top store the index of the first,second,thrid...Athlete
+	 */
+	public static int[] summarise(ArrayList<Double> list){
+		int i = 0;
+		ArrayList<Double> copy = new ArrayList<Double>();
+		for(double b : list){
+			copy.add(b);
+			i++;
+		}
+		int[] top = new int[i];
+		copy.sort(null);
+		
+		for(int j = 0; j < i ;j++){
+			top[j] = list.indexOf(copy.get(j));
+		}
+				
+		return top;
+	}
+	
+}
---
+++ SwimGame.java
@@ -0,0 +1,20 @@
+/*swimGame*/
+public class SwimGame extends Game{
+	SwimGame(int i){
+		this.setID("S0" + i);
+	}
+}
+
+/*RunGame*/
+class RunGame extends Game{
+	RunGame(int i){
+		this.setID("R0" + i);
+	}
+}
+
+/*ClalyGame*/
+class ClclyGame extends Game{
+	ClclyGame(int i){
+		this.setID("C0" + i);
+	}
+}
